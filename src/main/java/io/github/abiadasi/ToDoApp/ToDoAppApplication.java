c05ef10cee0cfb9eb6cf949dbd01001d682101c2
adamb <adam.biadasiewicz@gmail.com>
2022-02-16 20:42:11 +0100

    Initial commit

---
+++ src/main/java/io/github/abiadasi/ToDoApp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.abiadasi.ToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
