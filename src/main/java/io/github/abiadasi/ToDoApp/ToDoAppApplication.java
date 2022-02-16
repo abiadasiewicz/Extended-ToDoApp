7547cd22e30e12dc4e1be05faae59d92b605ddec
adamb <adam.biadasiewicz@gmail.com>
2022-02-16 22:17:42 +0100

    16.02 - Create a repo

--- src/main/java/io/github/abiadasi/ToDoApp/ToDoAppApplication.java
+++ src/main/java/io/github/abiadasi/ToDoApp/ToDoAppApplication.java
@@ -2,12 +2,26 @@ package io.github.abiadasi.ToDoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
+	@Bean
+	Validator validator(){
+		return new LocalValidatorFactoryBean();
+	}
 
+	@Override
+	public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingListener) {
+		validatingListener.addValidator("BeforeCreate", validator());
+		validatingListener.addValidator("BeforeSave", validator());
+	}
 }
